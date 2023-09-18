c7755ba2c25ad76ed4e5e143a1ec9b4797411b24
Jr <jubayerrahman203@gmail.com>
2023-09-18 22:15:45 +0600

    himplisitiC

--- DaTaTypes/DT_Conversion.c
+++ DaTaTypes/DT_Conversion.c
@@ -2,8 +2,12 @@
 
 int main (void) {
 
-  char a = "c";
+  char a = 'c';
   printf("difftrent Value of A - %d, %x, %c", a,a,a);
+
+  // implisit conversion
+  int r = 'a' + 2;
+  printf("result of a + 2 : %d", r);
 
   return 0;
 }
