4637a8fe4cd3b04cc7d5c13a1cdc891653e01a1c
Jr <jubayerrahman203@gmail.com>
2023-09-18 22:16:14 +0600

    himplisiaddline brktiC

--- DaTaTypes/DT_Conversion.c
+++ DaTaTypes/DT_Conversion.c
@@ -3,11 +3,11 @@
 int main (void) {
 
   char a = 'c';
-  printf("difftrent Value of A - %d, %x, %c", a,a,a);
+  printf("difftrent Value of A - %d, %x, %c\n", a,a,a);
 
   // implisit conversion
   int r = 'a' + 2;
-  printf("result of a + 2 : %d", r);
+  printf("result of a + 2 : %d\n", r);
 
   return 0;
 }
