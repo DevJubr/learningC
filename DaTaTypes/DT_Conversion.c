828fd77cb843d57bf35059f465999f15f3bed947
Jr <jubayerrahman203@gmail.com>
2023-09-18 22:24:03 +0600

    himpliexplisit conversitiC

--- DaTaTypes/DT_Conversion.c
+++ DaTaTypes/DT_Conversion.c
@@ -9,5 +9,10 @@ int main (void) {
   int r = 'a' + 2;
   printf("result of a + 2 : %d\n", r);
 
+  // explisit conversion
+  //  here i expect float data, but my dicliarde dataType is int. so here when i try to print i tell that int var thet u become float. thsi is called explisit convertion.
+  int rF = 'a' + 2;
+  printf("result of Rf + 2 : %f\n", (float)rF);
+
   return 0;
 }
