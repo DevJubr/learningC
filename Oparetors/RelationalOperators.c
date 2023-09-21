746e3a864b6374c36ab8e4e75971e3d2e6b5d476
Jr <jubayerrahman203@gmail.com>
2023-09-21 15:12:27 +0600

    addSmeet

---
+++ Oparetors/RelationalOperators.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int main () {
+int a = 10;
+int b = 20;
+
+// = = (equal to): Checks if two operands are equal.
+bool et = a == b;
+printf("a is equal to b - %d\n", et);
+
+    return 0;
+}
