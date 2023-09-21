c91a5d391d456500e7e840d3fb68962ff6dd0824
Jr <jubayerrahman203@gmail.com>
2023-09-21 15:19:31 +0600

    addSmlstfeet

--- Oparetors/RelationalOperators.c
+++ Oparetors/RelationalOperators.c
@@ -10,18 +10,21 @@ int b = 20;
 bool et = a == b;
 printf("a is equal to b - %d\n", et);
 
-// ! = (not equal to): Checks if two operands are not equal.
+// ! = ( equal to): Checks if two operands are not equal.
 bool net = a != b;
-printf("a is equal to b - %d\n", net);
+printf("a is not equal to b - %d\n", net);
 
 // < (less than): Checks if the left operand is less than the right operand.
 bool lst = a < b;
 printf("a is equal to b - %d\n", lst);
 
 // > (greater than): Checks if the left operand is less than the right operand.
-bool gtt = a > b;
+bool gtt = b > a;
 printf("a is equal to b - %d\n", gtt);
 
+// <= (less than or equal to): Checks if the left operand is less than or equal to the right operand.
+bool ltort = a <= b;
+printf("a is equal to b - %d\n", ltort);
 
     return 0;
 }
