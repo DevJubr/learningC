2d6faa244bb2107c48b6ddc9015469a4b07aae01
Jr <jubayerrahman203@gmail.com>
2023-09-23 15:31:34 +0600

    addStfsize of charn trnry

---
+++ Oparetors/SizeofOperators.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+
+  char c = 'A';
+  short s = 43;
+  int i = 231;
+  long l = 3232l;
+  long long ll = 2343ll;
+
+
+  printf("Sizeof char - %d\n", (int)sizeof(c));
+
+    return 0;
+}
