4c50db94f5bf73c2c3099ba9b9f7da751b54fda4
Jr <jubayerrahman203@gmail.com>
2023-09-28 12:35:54 +0600

    add else if doc  condition

--- Condition in C/ElseIf.c
+++ Condition in C/ElseIf.c
@@ -19,3 +19,11 @@ int main() {
 
     return 0;
 }
+
+
+//  -  We include the stdio.h library for printing, as usual.
+//  -  We declare and initialize an integer variable number with the value 7.
+//  -  Inside the if statement, we first check if number is greater than 10. If this condition is true, it prints "The number is greater than 10."
+//  -  If the first condition is false, the program moves to the else if part and checks if number is equal to 10. If this condition is true, it prints "The number is equal to 10."
+//  -  If none of the previous conditions are true, the program executes the code inside the else block, printing "The number is less than 10."
+//  -  The program then continues executing after the if-else-if-else block.
