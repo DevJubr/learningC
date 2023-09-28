51670ac8d349f7322816566fedd3fdd62587ff5f
Jr <jubayerrahman203@gmail.com>
2023-09-28 12:33:11 +0600

    add else  condition

---
+++ Condition in C/Else.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int number = 3;
+
+    // Check if the 'number' variable is greater than 5
+    if (number > 5) {
+        // This block will execute if the condition is true
+        printf("The number is greater than 5.\n");
+    } else {
+        // This block will execute if the condition is false
+        printf("The number is not greater than 5.\n");
+    }
+
+    // The program continues here after the 'if-else' block
+
+    return 0;
+}
