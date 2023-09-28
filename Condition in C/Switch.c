4b5e26636ce3ba99b45b964626528a11cd35ca91
Jr <jubayerrahman203@gmail.com>
2023-09-28 12:38:29 +0600

    add switch case  condition

---
+++ Condition in C/Switch.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int dayNumber = 1;
+
+    switch (dayNumber) {
+        case 1:
+            printf("Sunday\n");
+            break;
+        case 2:
+            printf("Monday\n");
+            break;
+        case 3:
+            printf("Tuesday\n");
+            break;
+        case 4:
+            printf("Wednesday\n");
+            break;
+        case 5:
+            printf("Thursday\n");
+            break;
+        case 6:
+            printf("Friday\n");
+            break;
+        case 7:
+            printf("Saturday\n");
+            break;
+        default:
+            printf("Invalid day number\n");
+            break;
+    }
+
+    return 0;
+}
