7726db418c9b4b9936480ebd14e601203cda87f6
Jr <jubayerrahman203@gmail.com>
2023-09-28 12:33:59 +0600

    addelseDocf condition

--- Condition in C/Else.c
+++ Condition in C/Else.c
@@ -16,3 +16,12 @@ int main() {
 
     return 0;
 }
+
+
+// -  We still include the stdio.h library for printing.
+// -  We declare and initialize an integer variable number with the value 3.
+// -  Inside the if statement, we check if number is greater than 5.
+// -  If the condition (number > 5) is true, the code inside the if block will execute, and it will print "The number is greater than 5."
+// -  If the condition is false, the code inside the else block will execute instead, and it will print "The number is not greater than 5."
+// -  The program will then continue executing after the if-else block.
+// -  The else block provides an alternative set of instructions to execute when the if condition is false, giving you more control over the flow of your program.
