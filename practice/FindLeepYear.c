9343a81ec5255962137d095b02d6104196e8df5c
Jr <jubayerrahman203@gmail.com>
2023-10-02 22:24:01 +0600

    adding som addbdfsrfe practice

---
+++ practice/FindLeepYear.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+
+int num;
+printf("enter a number: ");
+scanf("%d", &num);
+
+if ((num % 4 == 0 && num % 100 !=0 )|| num %400 == 0){
+printf("%d is leep year");
+
+}
+
+
+
+}
