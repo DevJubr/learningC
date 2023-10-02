5cb56ae8df0bb3d00ca87300bf8f4a50ac0e426c
Jr <jubayerrahman203@gmail.com>
2023-10-02 21:59:29 +0600

    adding some pin vairactice

--- practice/EnemAndOdd.c
+++ practice/EnemAndOdd.c
@@ -6,6 +6,12 @@ int num;
 printf("enter a number: ");
 scanf("%d", &num);
 
-printf("%d", num);
+if(num%2 == 0) {
+  printf("%d is even num", num);
+} else {
+  printf("%d is odd num", num);
+
+}
+
 
 }
