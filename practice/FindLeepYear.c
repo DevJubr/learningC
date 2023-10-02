ce141ecafd745451c1fa8151fe856e17399e12d1
Jr <jubayerrahman203@gmail.com>
2023-10-02 22:27:03 +0600

    adding som addbtthdhtderyht55e practice

--- practice/FindLeepYear.c
+++ practice/FindLeepYear.c
@@ -10,7 +10,7 @@ if ((num % 4 == 0 && num % 100 !=0 )|| num %400 == 0){
 printf("%d is leep year\n", num);
 
 } else{
-printf("%d is not leep year", num);
+printf("%d is not leep year\n", num);
 
 }
 
