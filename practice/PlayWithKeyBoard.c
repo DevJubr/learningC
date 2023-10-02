41efd635e9dcc446941675a43711a79cda78e97a
Jr <jubayerrahman203@gmail.com>
2023-10-02 22:09:00 +0600

    adding som addbe practice

--- practice/PlayWithKeyBoard.c
+++ practice/PlayWithKeyBoard.c
@@ -6,7 +6,12 @@ int num;
 printf("enter a number: ");
 scanf("%d", &num);
 
+if( num >= 48 && num <= 57){
+  printf("%d is a Number", num);
+} else if(num >= 65 && num <= 90){
+  printf("%c is a Uppercase letter", num);
 
+}
 
 
 }
