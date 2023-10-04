3a3961bee460bacfffb3ab1d01a155c080066ceb
Jr <jubayerrahman203@gmail.com>
2023-10-04 22:08:53 +0600

    fdsadd a

--- loop/for.c
+++ loop/for.c
@@ -9,14 +9,14 @@ for (i = 0; i <= 40; i++)
 
 // lets find a odd oand even number
 
-  for ( i = 1; i <= 10; i++)
-  {
-   if (i%2 == 0)
-   {
-    printf("%d\n", i);
-   } 
+  // for ( i = 1; i <= 10; i++)
+  // {
+  //  if (i%2 == 0)
+  //  {
+  //   printf("%d\n", i);
+  //  } 
    
-  }
+  // }
   
 
   return 0;
