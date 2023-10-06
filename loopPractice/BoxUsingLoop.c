15fad5e35efaf66c7d4cbfcf2f92e42c957da652
Jr <jubayerrahman203@gmail.com>
2023-10-06 14:44:23 +0600

    ading newbribhuj 90d kon  code

--- loopPractice/BoxUsingLoop.c
+++ loopPractice/BoxUsingLoop.c
@@ -9,8 +9,9 @@ int main (void) {
 
   int col = 5;
   int row = 4;
+  int n = 14;
 
-  int i,j;
+  int i,j,k;
 
   // for (i=0; i< col; i++) {
   //   for(j=0; j<row; j++) {
@@ -41,13 +42,40 @@ int main (void) {
   // * *
   // *
 
-  for(i = 0; i < col; i++){
-    for(j=i; j < row; j++){
-      printf(" * ");
+  // for(i = 0; i < col; i++){
+  //   for(j=i; j < row; j++){
+  //     printf(" * ");
+  //   }
+
+  //   printf("\n");
+  // }
+
+
+  // -> Now, we will create this box shape using loops.
+  //    *  
+  //   * *
+  //  * * *
+  // * * * *
+  //  * * * 
+  //   * *
+  //    *
+
+   for (int i = 1; i <= n; i++) {
+        // Print spaces for alignment
+        for (int j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+
+        // Print asterisks
+        for (int k = 1; k <= i; k++) {
+            printf("* ");
+        }
+
+        // Move to the next line
+        printf("\n");
     }
 
-    printf("\n");
-  }
+
 
     return 0;
 }
