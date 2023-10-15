9a0ef15a86dbb8e9f3f56779040675a1bfcc54a7
Jr <jubayerrahman203@gmail.com>
2023-10-15 11:58:39 +0600

    added something

---
+++ Array/HelloArray.c
@@ -0,0 +1,22 @@
+#include <stdio.h> // Include the standard input/output library.
+
+int main() {
+    // Declare an array of integers with a size of 5.
+    int numbers[5];
+
+    // Initialize the array with values.
+    numbers[0] = 10;
+    numbers[1] = 20;
+    numbers[2] = 30;
+    numbers[3] = 40;
+    numbers[4] = 50;
+
+    // Access and print the elements of the array.
+    printf("Element 1: %d\n", numbers[0]);
+    printf("Element 2: %d\n", numbers[1]);
+    printf("Element 3: %d\n", numbers[2]);
+    printf("Element 4: %d\n", numbers[3]);
+    printf("Element 5: %d\n", numbers[4]);
+
+    return 0; // Return 0 to indicate successful program execution.
+}
